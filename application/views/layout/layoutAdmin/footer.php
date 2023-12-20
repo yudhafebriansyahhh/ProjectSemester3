6d2b7b21b6a54c845faa7dae109228efa1376a42
Yudha Febriansyah <yudha22ti@mahasiswa.pcr.ac.id>
2023-12-20 08:36:04 +0700

    Template Admin

--- application/views/layout/layoutAdmin/footer.php
+++ application/views/layout/layoutAdmin/footer.php
@@ -1,13 +1,26 @@
 </div>
-    </div>
-  </div>
-  <script src="<?= base_url('assets/') ?>/assets/libs/jquery/dist/jquery.min.js"></script>
-  <script src="<?= base_url('assets/') ?>/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="<?= base_url('assets/') ?>/assets/js/sidebarmenu.js"></script>
-  <script src="<?= base_url('assets/') ?>/assets/js/app.min.js"></script>
-  <script src="<?= base_url('assets/') ?>/assets/libs/apexcharts/dist/apexcharts.min.js"></script>
-  <script src="<?= base_url('assets/') ?>/assets/libs/simplebar/dist/simplebar.js"></script>
-  <script src="<?= base_url('assets/') ?>/assets/js/dashboard.js"></script>
-</body>
 
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+
+    <script src="<?= base_url('assets')?>/Admin/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="<?= base_url('assets')?>/Admin/assets/vendor/libs/popper/popper.js"></script>
+    <script src="<?= base_url('assets')?>/Admin/assets/vendor/js/bootstrap.js"></script>
+    <script src="<?= base_url('assets')?>/Admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="<?= base_url('assets')?>/Admin/assets/vendor/js/menu.js"></script>
+
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="<?= base_url('assets')?>/Admin/assets/vendor/libs/apex-charts/apexcharts.js"></script>
+
+    <!-- Main JS -->
+    <script src="<?= base_url('assets')?>/Admin/assets/js/main.js"></script>
+
+    <!-- Page JS -->
+    <script src="<?= base_url('assets')?>/Admin/assets/js/dashboards-analytics.js"></script>
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+  </body>
 </html>
