694fd629b0c3e3493d0c34f57101815ac5de57c5
ArifFathurR <138746364+ArifFathurR@users.noreply.github.com>
2023-12-20 07:31:30 +0700

    commit

--- application/views/layout/landingPage/header.php
+++ application/views/layout/landingPage/header.php
@@ -51,9 +51,9 @@
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href=<?= site_url('Welcome/')?>>Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="nav-link scrollto" href="#contact">History</a></li>
+          <li><a class="nav-link scrollto" href="#services">Saldo</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Lokasi</a></li>
           <li><a class="getstarted scrollto" href="<?= base_url('Auth')?>">Login</a></li>
           <li><a class="getstarted scrollto" href="<?= base_url('Auth/logout')?>">Log out</a></li>
         </ul>
