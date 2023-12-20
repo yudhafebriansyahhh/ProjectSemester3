7af5e9e00047f8ca22806b3dd3f5a358bdd3b5b4
Yudha Febriansyah <yudha22ti@mahasiswa.pcr.ac.id>
2023-12-20 10:34:12 +0700

    Tampilan Tabel User

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -7,13 +7,24 @@ class Admin extends CI_Controller
   public function __construct()
   {
     parent::__construct();
+    $this->load->model('Nasabah_model');
   }
 
   public function index()
   {
     $this->load->view("layout/layoutAdmin/header");
-      $this->load->view("admin/transaksi");
-      $this->load->view("layout/layoutAdmin/footer");
+    $this->load->view("admin/dashboard");
+    $this->load->view("layout/layoutAdmin/footer");
+  }
+  
+  public function userData()
+  {
+    $data['judul'] = "Data Table User";
+    $data['admin'] = $this->db->get_where('admin', ['email' => $this->session->userdata('email')])->row_array();
+    $data['nasabah'] = $this->Nasabah_model->get();
+    $this->load->view("layout/layoutAdmin/header");
+    $this->load->view("admin/dataUser", $data);
+    $this->load->view("layout/layoutAdmin/footer");
   }
 
 }
